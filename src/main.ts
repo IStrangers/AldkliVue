28eb15c6aa22deecb4d2f57bebf3f35806a314fd
xjh <qqq000qqq@aliyun.com>
2023-08-24 23:13:06 +0800

    20230824

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import ViewUIPlus from 'view-ui-plus'
+import 'view-ui-plus/dist/styles/viewuiplus.css'
+
+const app = createApp(App)
+
+app
+.use(router)
+.use(ViewUIPlus)
+
+app.mount('#app')
---
+++ src/views/HomeView.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { reactive } from 'vue'
+const apiGroupMetaDataList = reactive([])
+</script>
+
+<template>
+    <Row>
+        
+    </Row>
+    <Row>
+        <Col span="12">
+            <Menu v-for="(apiGroupMetaData,groupIndex) in apiGroupMetaDataList" active-name="1-2" :open-names="['1']">
+                <Submenu :name="groupIndex">
+                    <template #title>
+                        <Icon type="ios-analytics" />
+                        {{ apiGroupMetaData.value }}
+                    </template>
+                    <MenuItem v-if="(apiMetaData,apiIndex) in apiGroupMetaData.apiMetaDataList" :name="`${groupIndex}-${apiIndex}`">
+                        {{ apiMetaData.value }}
+                    </MenuItem>
+                </Submenu>
+            </Menu>
+        </Col>
+        <Col span="12">
+
+        </Col>
+    </Row>
+</template>
+
+<style scoped>
+
+</style>
