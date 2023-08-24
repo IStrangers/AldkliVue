72331b46cec62e1068386cf22c9a002ace5ec90a
xjh <qqq000qqq@aliyun.com>
2023-08-25 00:07:16 +0800

    20230825

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,32 +1,34 @@
 <script setup lang="ts">
 import { reactive } from 'vue'
-const apiGroupMetaDataList = reactive([])
+const apiGroupMetaDataList = [{"apiMetaDataList":[{"methodType":"ALL","name":"获取测试列表","pathList":["getTestList","TestList"]},{"methodType":"GET","name":"getMapping","pathList":["getMapping"]}],"name":"测试分组","pathList":["test"]}]
 </script>
 
 <template>
     <Row>
-        
-    </Row>
-    <Row>
-        <Col span="12">
-            <Menu v-for="(apiGroupMetaData,groupIndex) in apiGroupMetaDataList" active-name="1-2" :open-names="['1']">
-                <Submenu :name="groupIndex">
+        <Col>
+            <Menu active-name="0-0" :open-names="['0']">
+                <Submenu v-for="(apiGroupMetaData,groupIndex) in apiGroupMetaDataList" :name="`${groupIndex}`">
                     <template #title>
                         <Icon type="ios-analytics" />
-                        {{ apiGroupMetaData.value }}
+                        {{ apiGroupMetaData.name }}
                     </template>
-                    <MenuItem v-if="(apiMetaData,apiIndex) in apiGroupMetaData.apiMetaDataList" :name="`${groupIndex}-${apiIndex}`">
-                        {{ apiMetaData.value }}
+                    <MenuItem v-for="(apiMetaData,apiIndex) in apiGroupMetaData.apiMetaDataList" :name="`${groupIndex}-${apiIndex}`">
+                        {{ apiMetaData.name }}
                     </MenuItem>
                 </Submenu>
             </Menu>
         </Col>
-        <Col span="12">
+        <Col>
 
         </Col>
     </Row>
 </template>
 
-<style scoped>
-
+<style scoped lang="scss">
+.ivu-row {
+    height: 100%;
+    .ivu-menu {
+        height: 100%;
+    }
+}
 </style>
